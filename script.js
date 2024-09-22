1d81661f30df043e5f156c0d2ee51463ff70e329
Vitor Presot <129711394+VitorPresot@users.noreply.github.com>
2024-09-22 20:58:30 -0300

    De tudo um pouco front e back

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('open');
 }
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    
+    section.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
